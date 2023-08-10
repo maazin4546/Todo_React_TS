6f2e47a0c2e71570dcfc172f503aeb4b16a1c66a
Maazin Abdullah <mazinabdullah4546@gmail.com>
2023-08-10 22:37:14 +0800

    first

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import AddtoDo from "./components/addtodo";
+import Navbar from "./components/Navbar";
+import Todos from "./components/Todos";
+import "./App.css";
+
+const App = () => {
+  return (
+    <main>
+      <h1 style={{marginBottom:"40px"}}>TODO REACT + TYPESCRIPT </h1>
+      <Navbar />
+      <AddtoDo />
+      <Todos />
+    </main>
+  );
+};
+
+export default App;
